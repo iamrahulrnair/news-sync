ebcaf33612d7977171b0bee9e73a27d4cb94ed6e
iamrahulrnair <rahulrnair007@outlook.com>
2021-11-14 20:03:17 +0530

    add copyright footer

---
+++ container/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className="w-full h-20 flex flex-row justify-center items-center">
+      <div className="p-2 text-blue-500 text-xs">
+        © 2021 Copyright: <a href="https://github.com/iamrahulrnair">Rahul R</a>
+      </div>
+    </div>
+  );
+}
